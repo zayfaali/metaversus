f173c5bd95c87c99b3faa1a6fd3415428ade92d9
zayfaali <huzaifaalix@gmail.com>
2023-08-29 00:37:35 +0500

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -60,7 +60,7 @@ module.exports = {
     "jsx-a11y/no-autofocus": 0,
     "jsx-a11y/no-static-element-interactions": 0,
     "react/no-array-index-key": 0,
-    quotes: [2, "single", { avoidEscape: true }],
+    quotes: [2, "double"],
     "jsx-a11y/anchor-is-valid": [
       "error",
       {
